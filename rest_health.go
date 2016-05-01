663145620c7de6aa0654f472dfd0f78a947e94e9
Ingo Jaeckel <ijaeckel@ea.com>
2016-05-01 15:17:09 -0400

    added memcache connectivity to health check

--- rest_health.go
+++ rest_health.go
@@ -3,9 +3,21 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/bradfitz/gomemcache/memcache"
 )
 
 func getHealth(w http.ResponseWriter, r *http.Request) {
-	// TODO add memcache connectivity
+	item := &memcache.Item{
+		Key:   "health_check",
+		Value: []byte{},
+	}
+	// check memcache connectivity
+	if err := memcacheClient.Set(item); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "memcache connectivity: %s", err.Error())
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, "ok")
 }
