8a2a92f328d6b9c3dd894e86443211f2c0c07bce
Ingo Jaeckel <ijaeckel@ea.com>
2016-05-02 10:50:05 -0400

    reduced repetition within getRecommendation code

--- rest_forecast.go
+++ rest_forecast.go
@@ -10,17 +10,11 @@ func getForecast(w http.ResponseWriter, r *http.Request) {
 	q := r.URL.Query()
 	c, ok := q["cityId"]
 	if !ok {
-		w.WriteHeader(http.StatusBadRequest)
-		result := RecommendationResponse{Status: 1, Error: "Missing cityId"}
-		w.Header().Add("Content-Type", "application/json")
-		io.WriteString(w, result.String())
+		writeJSONResponse(w, http.StatusBadRequest, RecommendationResponse{Status: 1, Error: "Missing cityId"})
 		return
 	}
 	if len(c) != 1 {
-		w.WriteHeader(http.StatusBadRequest)
-		result := RecommendationResponse{Status: 2, Error: fmt.Sprintf("Invalid number of cityIds: %d", len(c))}
-		w.Header().Add("Content-Type", "application/json")
-		io.WriteString(w, result.String())
+		writeJSONResponse(w, http.StatusBadRequest, RecommendationResponse{Status: 2, Error: fmt.Sprintf("Invalid number of cityIds: %d", len(c))})
 		return
 	}
 
@@ -33,24 +27,23 @@ func getForecast(w http.ResponseWriter, r *http.Request) {
 
 	s, err := getWeatherProvider()
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		result := RecommendationResponse{Status: 3, Error: err.Error()}
-		w.Header().Add("Content-Type", "application/json")
-		io.WriteString(w, result.String())
+		writeJSONResponse(w, http.StatusInternalServerError, RecommendationResponse{Status: 3, Error: err.Error()})
 		return
 	}
 
 	resp, err := s.GetWeather(cityID)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		result := RecommendationResponse{Status: 4, Error: err.Error()}
-		w.Header().Add("Content-Type", "application/json")
-		io.WriteString(w, result.String())
+		writeJSONResponse(w, http.StatusInternalServerError, RecommendationResponse{Status: 4, Error: err.Error()})
 		return
 	}
 
-	result := RecommendationResponse{Recommendation: GetRecommendation(resp).String()}
+	val := GetRecommendation(resp).String()
+	writeJSONResponse(w, http.StatusOK, RecommendationResponse{Recommendation: val})
+	cachePut(cityID, val, forecastCacheTTLSeconds)
+}
+
+func writeJSONResponse(w http.ResponseWriter, status int, r RecommendationResponse) {
+	w.WriteHeader(status)
 	w.Header().Add("Content-Type", "application/json")
-	io.WriteString(w, result.String())
-	cachePut(cityID, result.String(), forecastCacheTTLSeconds)
+	io.WriteString(w, r.String())
 }
