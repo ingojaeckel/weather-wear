586661126a94aab9544676849f4ffd2d3120cf5f
Ingo Jaeckel <ijaeckel@ea.com>
2016-05-09 13:03:42 -0700

    Ensure error is logged if app fails to startup e.g. due to port already being in use

--- app.go
+++ app.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -13,5 +14,5 @@ func main() {
 
 	initializeCache()
 
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
