bdc0fd16ba16fc9a9f4fd8fc2b0d2997bb86ea71
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2016-05-16 21:02:36 -0700

    Added building docker container to script

--- modules/binary-test/buildAndRun.sh
+++ modules/binary-test/buildAndRun.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+
 # Builds the app for a Linux docker container
 CGO_ENABLED=0 GOOS=linux go build -ldflags "-s" -a -installsuffix cgo
 mv binary-test entrypoint
+
+# Build and run docker container
+docker build -t docker-test .
+docker run -i -t -p 8080:8080 docker-test
