cc520b064f0d6cdf9ad44971fcb05e523c9e0f7d
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2016-05-16 21:14:52 -0700

    added tcp connect example code with timeout

--- modules/binary-test/app.go
+++ modules/binary-test/app.go
@@ -3,16 +3,26 @@ package main
 import (
 	"fmt"
 	"log"
+ 	"net"
 	"net/http"
+	"time"
 )
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, "Hello World!")
+}
+
+func connect(w http.ResponseWriter, r *http.Request) {
+	if _, err := net.DialTimeout("tcp", "146.148.87.97:8125", 5 * time.Second); err != nil {
+		fmt.Fprint(w, err.Error())
+		return
+	}	
+	fmt.Fprint(w, "Connected")
 }
 
 func main() {
 	http.HandleFunc("/hello", helloWorld)
+	http.HandleFunc("/connect", connect)
 	fmt.Println("Running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
