c1c34b1ae18f62aef78b3f5af6a71e7d9f873af5
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2016-05-17 20:03:18 -0700

    added background service that can report the external IP address over HTTP

--- modules/docker-dd-agent/deploy.sh
+++ modules/docker-dd-agent/deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+# Build the golang app
+./build.sh
 gcloud preview app deploy app.yaml --promote --version 1
---
+++ modules/docker-dd-agent/start.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+chmod 755 /background-service
+nohup sh -c /background-service &
